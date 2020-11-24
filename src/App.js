48b678c9482c815e9c1ed470cac52be3dc131530
Jisan Jung <jjjung13@gmail.com>
2020-11-24 00:03:42 -0500

    slick js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import Button from './components/Button';
 import "./sass/main.scss";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <Button text="SEND"/>
-        <FontAwesomeIcon icon={faCoffee} />
+        <h1>Hi</h1>
       </div>
     )
   }
