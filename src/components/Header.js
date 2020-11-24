71099adb566179141eb5c0b73e657a1acc32884b
Jisan Jung <jjjung13@gmail.com>
2020-11-24 11:26:27 -0500

    container styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,22 +19,24 @@ const Header = () => {
 
     return (
         <header>
-            <div className="flex align-center flex-between">
-                <div className="flex align-center">
-                    <div className="logo">
-                        <img src={logo} alt="logo"/>
+            <div className="container">
+                <div className="flex align-center flex-between">
+                    <div className="flex align-center">
+                        <div className="logo">
+                            <img src={logo} alt="logo"/>
+                        </div>
+                        <div className="logo-text">
+                            <img src={whitetext} alt="logo text"/>
+                        </div>
                     </div>
-                    <div className="logo-text">
-                        <img src={whitetext} alt="logo text"/>
-                    </div>
+                    <button type="button" onClick={toggleMobileNav} className="menu-btn">
+                        <FontAwesomeIcon icon={faBars} style={{
+                            color: "#fff",
+                            fontSize: "1.5rem"
+                        }}/>
+                    </button>
+                    <Nav/>
                 </div>
-                <button type="button" onClick={toggleMobileNav} className="menu-btn">
-                    <FontAwesomeIcon icon={faBars} style={{
-                        color: "#fff",
-                        fontSize: "1.5rem"
-                    }}/>
-                </button>
-                <Nav/>
             </div>
             <MobileNav display={mobileNav}/>
         </header>
