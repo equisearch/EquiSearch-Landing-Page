079a5f4813151fa91568c8732dfede338f408ccd
Jisan Jung <jjjung13@gmail.com>
2021-08-13 15:01:03 -0400

    test react hook form usage

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import Header from '../sections/Header'
-import Modal from '../utils/Modal'
+import Header from '../sections/Header';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div>
             <Header/>
-            login
+            <form onSubmit={handleSubmit(onSubmit)} style={{padding: "100px"}}> 
+                <input defaultValue="test" {...register("example")}/>
+                <input {...register("exampleRequired", { required: true })} />
+                {errors.exampleRequired && <span>This field is required</span>}
+                <input type="submit" />
+            </form>
         </div>
     )
 }
